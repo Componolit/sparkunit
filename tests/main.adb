7f1de74a501a2595f12e657adbe963c6cb4d4ce9
Alexander Senier <mail@senier.net>
2010-09-19 23:40:17 +0200

    Extend test.

--- tests/main.adb
+++ tests/main.adb
@@ -13,11 +13,22 @@ is
    subtype My_Harness_Index is Natural range 1 .. 100;
    subtype My_Harness_Type is SPARKUnit.Harness_Type (My_Harness_Index);
 
-   My_Harness : My_Harness_Type;
-   Suite1     : SPARKUnit.Index_Type;
+   My_Harness             : My_Harness_Type;
+   Suite1, Suite2, Suite3 : SPARKUnit.Index_Type;
 begin
-   SPARKUnit.Initialize (My_Harness, "My harness");
+   SPARKUnit.Create_Harness (My_Harness, "My harness");
+
    SPARKUnit.Create_Suite (My_Harness, "My first suite", Suite1);
-   SPARKUnit.Test (My_Harness, Suite1, "My first test", True);
+   SPARKUnit.Create_Test (My_Harness, Suite1, "My first test", True);
+   SPARKUnit.Create_Test (My_Harness, Suite1, "My second test", False);
+
+   SPARKUnit.Create_Suite (My_Harness, "My second suite", Suite2);
+   SPARKUnit.Create_Test (My_Harness, Suite2, "My third test", True);
+   SPARKUnit.Create_Test (My_Harness, Suite2, "My fourth test", False);
+   SPARKUnit.Create_Test (My_Harness, Suite2, "My fifth test", True);
+
+   SPARKUnit.Create_Suite (My_Harness, "My third suite", Suite3);
+   SPARKUnit.Create_Test (My_Harness, Suite3, "My sixth test", False);
+
    SPARKUnit.Text_Report (My_Harness);
 end Main;
